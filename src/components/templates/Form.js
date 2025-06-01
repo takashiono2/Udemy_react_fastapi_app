46e70f2a069a392704bb484a238904d97297d2f0
takashiono2 <takashiono710@gmail.com>
2025-06-02 00:21:21 +0900

    section3_38

--- src/components/templates/Form.js
+++ src/components/templates/Form.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Link, useNavigate } from 'react-router-dom';
+import { Container, Box, Typography } from '@mui/material';
 
 const Form = () => {
   const [form, setForm] = React.useState({
@@ -28,70 +29,80 @@ const Form = () => {
   }
 
   return (
-    <div>
-      <form>
-        <label htmlFor='name'>
-          名前
+    <Container>
+      <Box
+        sx={{
+          marginTop: 1,
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+        }}
+      >
+        <Typography variant="h5">アンケート提出</Typography>
+        <div>
+          <label htmlFor='name'>
+            名前
+            <input
+              id="name"
+              type="text"
+              name="name"
+              value={form.name}
+              onChange={handleInputChange}
+            />
+          </label>
+          <br />
+          <label htmlFor='age'>
+            年齢
+            <select name="age" id="age" onChange={handleInputChange}>
+              <option value={10}>10代</option>
+              <option value={20}>20代</option>
+              <option value={30}>30代</option>
+            </select>
+          </label>
+          <br />
+          性別
           <input
-            id="name"
-            type="text"
-            name="name"
-            value={form.name}
+            type="radio"
+            name="gender"
+            id="male"
+            value="male"
             onChange={handleInputChange}
           />
-        </label>
-        <br />
-        <label htmlFor='age'>
-          年齢
-          <select name="age" id="age" onChange={handleInputChange}>
-            <option value={10}>10代</option>
-            <option value={20}>20代</option>
-            <option value={30}>30代</option>
-          </select>
-        </label>
-        <br />
-        性別
-        <input
-          type="radio"
-          name="gender"
-          id="male"
-          value="male"
-          onChange={handleInputChange}
-        />
-        男性
-        <input
-          type="radio"
-          name="gender"
-          id="female"
-          value="female"
-          onChange={handleInputChange}
-        />
-        女性
-        <input
-          type="radio"
-          name="gender"
-          id="other"
-          value="other"
-          onChange={handleInputChange}
-        />
-        その他
-        <br />
-        <label htmlFor='comment'>コメント
-          <textarea
-            name="comment"
-            placeholder="コメントを入力してください"
-            value={form.comment}
+          男性
+          <input
+            type="radio"
+            name="gender"
+            id="female"
+            value="female"
             onChange={handleInputChange}
-          >
-          </textarea>
-        </label>
-      </form>
-      button
-      <button
-        onClick={handleSubmit}
-      >送信</button>
-      <Link to="/result">結果ページへ</Link>
-    </div>
+          />
+          女性
+          <input
+            type="radio"
+            name="gender"
+            id="other"
+            value="other"
+            onChange={handleInputChange}
+          />
+          その他
+          <br />
+          <label htmlFor='comment'>コメント
+            <textarea
+              name="comment"
+              placeholder="コメントを入力してください"
+              value={form.comment}
+              onChange={handleInputChange}
+            >
+            </textarea>
+          </label>
+          button
+          <button
+            onClick={handleSubmit}
+          >送信</button>
+          <Link to="/result">結果ページへ</Link>
+        </div>
+      </Box>
+    </Container>
   )
 }
 
